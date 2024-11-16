b9e9e669a72c17e0fbd2830cbc9b1853c7d14adb
bowei <x.bowei@aim.com>
2024-11-16 23:48:45 +0100

    fix: deploy test v3

--- src/App.jsx
+++ src/App.jsx
@@ -7,23 +7,22 @@ function App() {
 
   return (
     <div className="container w-full">
-        {/*<Navigation />*/}
-        {/*<div className="grid grid-cols-2 sm:grid-cols-3 my-40">*/}
-        {/*    <HoverCard*/}
-        {/*        title="product #1"*/}
-        {/*        imgSrc='https://picsum.photos/480'>Description product #1</HoverCard>*/}
-        {/*    <HoverCard*/}
-        {/*        title="product #2"*/}
-        {/*        imgSrc='https://picsum.photos/580'>Description product #2</HoverCard>*/}
-        {/*    <HoverCard*/}
-        {/*        title="product #3"*/}
-        {/*        imgSrc='https://picsum.photos/680'>Description product #3</HoverCard>*/}
-        {/*    <HoverCard*/}
-        {/*        title="product #4"*/}
-        {/*        imgSrc='https://picsum.photos/780'>Description product #4</HoverCard>*/}
-        {/*</div>*/}
-        {/*<Footer />*/}
-        <h1>I'm Kong Bo.</h1>
+        <Navigation />
+        <div className="grid grid-cols-2 sm:grid-cols-3 my-40">
+            <HoverCard
+                title="product #1"
+                imgSrc='https://picsum.photos/480'>Description product #1</HoverCard>
+            <HoverCard
+                title="product #2"
+                imgSrc='https://picsum.photos/580'>Description product #2</HoverCard>
+            <HoverCard
+                title="product #3"
+                imgSrc='https://picsum.photos/680'>Description product #3</HoverCard>
+            <HoverCard
+                title="product #4"
+                imgSrc='https://picsum.photos/780'>Description product #4</HoverCard>
+        </div>
+        <Footer />
     </div>
   )
 }
